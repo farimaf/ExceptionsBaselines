f394232439a846f25946f4251d087fc4a211cc8d
farimaf <farahani.farima@gmail.com>
2021-01-23 00:29:41 -0800

    separating train and test

---
+++ src/utils/getTrainTestSet.java
@@ -0,0 +1,65 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.regex.Pattern;
+
+public class getTrainTestSet {
+    private static HashSet<String> trainSetNames=new HashSet<>();
+    private static HashSet<String> testSetNames=new HashSet<>();
+
+    public static void main(String[] args) {
+        if(args.length==3){
+            String trainsetPath=args[0];
+            String testsetPath=args[1];
+            String mainfilePath=args[2];
+            try {
+//                String[] trainFilePathSplit=trainsetPath.split(Pattern.quote(System.getProperty("file.separator")));
+//                String[] testFilePathSplit=testsetPath.split(Pattern.quote(System.getProperty("file.separator")));
+                String[] mainfilePathSplit=mainfilePath.split(Pattern.quote(System.getProperty("file.separator")));
+
+                PrintWriter pwTrain=new PrintWriter("Train_"+mainfilePathSplit[mainfilePathSplit.length-1]);
+                PrintWriter pwTest=new PrintWriter("Test_"+mainfilePathSplit[mainfilePathSplit.length-1]);
+
+                BufferedReader bf = new BufferedReader(new FileReader(trainsetPath));
+                String line="";
+                while ((line= bf.readLine())!=null){
+                    String[] lineSplit=line.split("@#@");
+                    String[] lineMethodSplit=lineSplit[1].split("#");
+                    trainSetNames.add(lineSplit[0]+"#"+lineMethodSplit[1]+"#"+lineMethodSplit[2]);
+                }
+
+                bf = new BufferedReader(new FileReader(testsetPath));
+                line="";
+                while ((line= bf.readLine())!=null){
+                    String[] lineSplit=line.split("@#@");
+                    String[] lineMethodSplit=lineSplit[1].split("#");
+                    testSetNames.add(lineSplit[0]+"#"+lineMethodSplit[1]+"#"+lineMethodSplit[2]);
+                }
+
+                bf = new BufferedReader(new FileReader(mainfilePath));
+                line="";
+                while ((line= bf.readLine())!=null){
+                    String[] lineSplit=line.split("@#@");
+                    String[] lineMethodSplit=lineSplit[1].split("#");
+                    String whatToLook=lineSplit[0]+"#"+lineMethodSplit[1]+"#"+lineMethodSplit[2];
+                    if(trainSetNames.contains(whatToLook)){
+                        pwTrain.write(line+System.lineSeparator());
+                    }
+                    else if(testSetNames.contains(whatToLook)){
+                        pwTest.write(line+System.lineSeparator());
+                    }
+                }
+
+                pwTest.close();
+                pwTrain.close();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}

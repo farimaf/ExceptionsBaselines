5610ba19cb8c15f003758db0a519797d432561a0
farimaf <farahani.farima@gmail.com>
2021-06-07 16:21:33 -0700

    random seed avoid repeations

--- src/Random/GetRandomSeed.java
+++ src/Random/GetRandomSeed.java
@@ -1,15 +1,18 @@
 package Random;
 
+import java.util.HashSet;
 import java.util.Random;
 
 public class GetRandomSeed {
     public static void main(String[] args) {
-        Random generator = new Random(100);
-        for (int i = 0; i <5 ; i++) {
-
-            int random_int = generator.nextInt();
-            System.out.println(random_int);
-
+        Random generator = new Random();
+        HashSet<Integer> randomSet=new HashSet<>();
+        while (randomSet.size()<5){
+            int random_int = generator.nextInt(100);
+            randomSet.add(random_int);
+        }
+        for (Integer i:randomSet) {
+            System.out.println(i);
         }
     }
 }
